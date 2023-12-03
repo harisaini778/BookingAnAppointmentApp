97b5af8a924407abe8a5f0e1635e485fc4136250
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-12-03 21:58:40 +0530

    my first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    return res.json("From Backend Side");
+});
+
+app.listen(8081, () => {
+    console.log("listening");
+})
